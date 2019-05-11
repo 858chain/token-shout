ab5ba74e54f8778b795856d0b8421cf3dd6fb465
Deshi Xiao <xiaods@gmail.com>
2019-05-11 08:30:09 +0800

    fix: remove unused ip from sample run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@
   --log-dir /tmp \
   start \
   --http-listen-addr 0.0.0.0:8081 \
-  --rpc-addr ws://107.150.126.20:9546 \
+  --rpc-addr ws://192.168.0.101:9546 \
   --receivers-conf-path /tmp/receivers \
   --watch-list eth,usdt,dusd \
   --eth-wallet-dir /tmp/wallets \
   --eth-watch-interval 20s \
   --erc20-contracts-dir /tmp/contracts
-
-#firewall-cmd --permanent --zone=public --add-rich-rule=' rule family="ipv4" source address="27.189.223.168/32" port protocol="tcp" port="9546" accept'
